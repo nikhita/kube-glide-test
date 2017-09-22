8696f33e5f413cb63746290ac2d9d2335a8d52d0
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-09-22 14:38:31 +0200

    Fix main.go

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
-	_ "k8s.io/kube-aggregator"
 	_ "k8s.io/apiserver/pkg/storage"
 	_ "k8s.io/client-go/kubernetes"
 	_ "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	_ "k8s.io/kube-aggregator/pkg/cmd/server"
+	_ "k8s.io/kube-aggregator/pkg/apis/apiregistration/install"
+	_ "k8s.io/kube-aggregator/pkg/apis/apiregistration/validation"
+	_ "k8s.io/kube-aggregator/pkg/client/clientset_generated/internalclientset"
+	_ "k8s.io/kube-aggregator/pkg/client/listers/apiregistration/internalversion"
+	_ "k8s.io/kube-aggregator/pkg/client/listers/apiregistration/v1beta1"
 )
 
 func main() {
